2f255674dd1e1465e0a0ff61a0e905efda158540
Kjeld Schmidt <contact@kjeld-schmidt.com>
2021-07-25 22:25:44 +0200

    Moved city lights start time even earlier - I increasingly have the feeling that even taking the time from the position of the sun in the sky might not accurately reflect on the brightness outside? (even ignoring obvious issues like cloud cover)

--- Woodlamp.py
+++ Woodlamp.py
@@ -67,7 +67,7 @@ class Woodlamp:
 	def schedule_sundown_lamp( self ) -> None:
 		sun_times_json = requests.get( 'https://api.sunrise-sunset.org/json?lat=51&lng=7&formatted=0' )
 		sun_times_times_utc = json.loads( sun_times_json.text )[ 'results' ]
-		twilight_start_string = sun_times_times_utc[ 'civil_twilight_end' ][ :-6 ]
+		twilight_start_string = sun_times_times_utc[ 'sunset' ][ :-6 ]
 		twilight_start_utc = parse_to_utc( twilight_start_string, '%Y-%m-%dT%H:%M:%S' )
 
 		self.next_sundown = local_time_today( twilight_start_utc )
