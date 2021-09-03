76aee4e21df7814d8f23bcd950169105cbea80b8
Kjeld Schmidt <contact@kjeld-schmidt.com>
2021-09-03 23:04:46 +0200

    Added `dim` setting for ceiling lights

--- CeilingLights.py
+++ CeilingLights.py
@@ -12,6 +12,7 @@ class CeilingLights:
 	def produce_main_page_content():
 		return f"""
 			<button onclick="fetch('ceiling/on')">On</button>
+			<button onclick="fetch('ceiling/dim')">Dim</button>
 			<button onclick="fetch('ceiling/off')">Off</button>
 		"""
 
@@ -20,12 +21,31 @@ class CeilingLights:
 		def ceiling_lights_on():
 			self.client.publish(
 				topic="zigbee2mqtt/0x680ae2fffe6a2ac5/set",
-				payload='{ "state": "ON" }'
+				payload='{ "brightness": "254" }'
+			)
+			self.client.publish(
+				topic="zigbee2mqtt/0xbc33acfffe59a606/set",
+				payload='{ "brightness": "254" }'
 			)
 
 		@app.route( '/ceiling/off' )
 		def ceiling_lights_off():
 			self.client.publish(
 				topic="zigbee2mqtt/0x680ae2fffe6a2ac5/set",
-				payload='{ "state": "OFF" }'
+				payload='{ "brightness": "0" }'
+			)
+			self.client.publish(
+				topic="zigbee2mqtt/0xbc33acfffe59a606/set",
+				payload='{ "brightness": "0" }'
+			)
+
+		@app.route( '/ceiling/dim' )
+		def ceiling_lights_dim():
+			self.client.publish(
+				topic="zigbee2mqtt/0x680ae2fffe6a2ac5/set",
+				payload='{ "brightness": "127" }'
+			)
+			self.client.publish(
+				topic="zigbee2mqtt/0xbc33acfffe59a606/set",
+				payload='{ "brightness": "127" }'
 			)
