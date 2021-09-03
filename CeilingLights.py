996abc4c7e3d46347235dc518114302a93174a6b
Kjeld Schmidt <contact@kjeld-schmidt.com>
2021-09-04 00:29:56 +0200

    Add precise color and temperature controls to ceiling lamps

--- CeilingLights.py
+++ CeilingLights.py
@@ -7,45 +7,39 @@ class CeilingLights:
 	def __init__( self, app: Flask ):
 		self.setup_routes( app )
 		self.client = MQTTClient.client
+		self.lamp_ids = [
+			'0x680ae2fffe6a2ac5',
+			'0xbc33acfffe59a606',
+		]
 
 	@staticmethod
 	def produce_main_page_content():
-		return f"""
-			<button onclick="fetch('ceiling/on')">On</button>
-			<button onclick="fetch('ceiling/dim')">Dim</button>
-			<button onclick="fetch('ceiling/off')">Off</button>
+		return """
+			<details>
+				<summary>
+					<button onclick="fetch('ceiling/brightness/254')">On</button>
+					<button onclick="fetch('ceiling/brightness/127')">Dim</button>
+					<button onclick="fetch('ceiling/brightness/0')">Off</button>
+				</summary> 
+				<br /><input type=range min=250 max=454 onchange="fetch(`ceiling/temp/${this.value}`)" />
+				<br /><input type=range min=0 max=254 onchange="fetch(`ceiling/brightness/${this.value}`)" />
+			</details>
 		"""
 
+	def send_to_all_lamps( self, payload ):
+		for lamp_id in self.lamp_ids:
+			self.client.publish(
+				topic=f"zigbee2mqtt/{lamp_id}/set",
+				payload=payload
+			)
+
 	def setup_routes( self, app: Flask ):
-		@app.route( '/ceiling/on' )
-		def ceiling_lights_on():
-			self.client.publish(
-				topic="zigbee2mqtt/0x680ae2fffe6a2ac5/set",
-				payload='{ "brightness": "254" }'
-			)
-			self.client.publish(
-				topic="zigbee2mqtt/0xbc33acfffe59a606/set",
-				payload='{ "brightness": "254" }'
-			)
+		@app.route( '/ceiling/brightness/<int:brightness>' )
+		def ceiling_lights_brightness( brightness: int ):
+			self.send_to_all_lamps( f'{{ "brightness": "{brightness}" }}' )
+			return "Ok", 200
 
-		@app.route( '/ceiling/off' )
-		def ceiling_lights_off():
-			self.client.publish(
-				topic="zigbee2mqtt/0x680ae2fffe6a2ac5/set",
-				payload='{ "brightness": "0" }'
-			)
-			self.client.publish(
-				topic="zigbee2mqtt/0xbc33acfffe59a606/set",
-				payload='{ "brightness": "0" }'
-			)
-
-		@app.route( '/ceiling/dim' )
-		def ceiling_lights_dim():
-			self.client.publish(
-				topic="zigbee2mqtt/0x680ae2fffe6a2ac5/set",
-				payload='{ "brightness": "127" }'
-			)
-			self.client.publish(
-				topic="zigbee2mqtt/0xbc33acfffe59a606/set",
-				payload='{ "brightness": "127" }'
-			)
+		@app.route( '/ceiling/temp/<int:color_temp>' )
+		def ceiling_lights_color( color_temp: int ):
+			self.send_to_all_lamps( f'{{ "color_temp": "{color_temp}" }}' )
+			return "Ok", 200
