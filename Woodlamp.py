1fc082eda6128589039c334fa9181b0649647443
Kjeld Schmidt <contact@kjeld-schmidt.com>
2021-09-04 17:13:26 +0200

    Cleaned up talking to multiple lamps

--- Woodlamp.py
+++ Woodlamp.py
@@ -1,19 +1,16 @@
-from datetime import datetime
-from datetime import datetime
+from threading import Thread
 from typing import Tuple, List
 
 import requests
 from flask import Flask
 
 from Scheduler import Scheduler
-from TimeFunctions import local_time_today
 
 
 class Woodlamp:
-	def __init__( self, app: Flask, scheduler: Scheduler, lamp_ip: List[str] ):
+	def __init__( self, app: Flask, scheduler: Scheduler, lamp_ips: List[str] ):
 		self.scheduler: Scheduler = scheduler
-		self.lamp_ips = lamp_ip
-		self.next_sundown: datetime = None
+		self.lamp_ips = lamp_ips
 		self.available_modes: List[ str ] = [ ]
 
 		self.setup( app )
@@ -53,18 +50,19 @@ class Woodlamp:
 		"""
 
 	def fetch_available_modes( self ) -> None:
-		response = requests.get( f'http://{self.lamp_ips[0 ]}/getModes' )
+		response = requests.get( f'http://{self.lamp_ips[ 1 ]}/getModes' )
 		mode_names = response.text.split( ',' )
 		mode_names = [ mode.strip() for mode in mode_names ]
 		self.available_modes = mode_names
 
 	def set_mode( self, mode: str ) -> Tuple[ str, int ]:
-		response = None
+		def set_individual_mode(ip):
+			requests.get( f'http://{ip}/setMode?newMode={mode}' )
+
 		for lamp_ip in self.lamp_ips:
-			response = requests.get( f'http://{lamp_ip}/setMode?newMode={mode}' )
-		if response is None:
-			return "No IPs", 500
-		return response.text, response.status_code
+			Thread(target=set_individual_mode, args=(lamp_ip,)).start()
+
+		return 'Ok', 200
 
 	def setup_routes( self, app: Flask ):
 		@app.route( '/woodlamp/mode/<string:mode>' )
