6846a808cff012e0f029107b128d65764c62d577
Kjeld Schmidt <contact@kjeld-schmidt.com>
2021-11-22 19:53:21 +0100

    Add ceiling to alarm

--- Server.py
+++ Server.py
@@ -3,9 +3,9 @@ from flask import Flask
 import MQTTClient
 from Alarm import Alarm
 from CeilingLights import CeilingLights
+from IkeaRemote import IkeaRemote
 from Scheduler import make_scheduler
 from Woodlamp import Woodlamp
-from IkeaRemote import IkeaRemote
 
 app = Flask( __name__ )
 make_scheduler( app )
@@ -13,12 +13,11 @@ make_scheduler( app )
 mqtt_client = MQTTClient.get_client()
 
 woodlamp = Woodlamp( app, [ '192.168.178.24', '192.168.178.28', '192.168.178.27' ] )
-alarm = Alarm( app, woodlamp )
 ceiling = CeilingLights( app, mqtt_client, [ '0x000d6ffffe35eed7' ] )
+alarm = Alarm( app, woodlamp, ceiling )
 controllers = [ woodlamp, alarm, ceiling ]
 
-IkeaRemote(ceiling, woodlamp, mqtt_client)
-
+IkeaRemote( ceiling, woodlamp, mqtt_client )
 
 style = """
 	<style>
