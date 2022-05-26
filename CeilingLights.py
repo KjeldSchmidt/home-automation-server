9ac077fbdc462875712dc0c54b17514a6e2d2e04
Kjeld Schmidt <contact@kjeld-schmidt.com>
2022-05-26 21:07:23 +0200

    Added clean support for multiple groups of ceiling lights

--- CeilingLights.py
+++ CeilingLights.py
@@ -4,29 +4,46 @@ import MqttClient
 from Woodlamp import Woodlamp
 
 
-class CeilingLights:
-	def __init__( self, app: Flask, mqtt_client: MqttClient.Client, woodlamp: Woodlamp ):
+class CeilingLightsCollection:
+	def __init__(self, lamp_config: dict[str, list[str]], app: Flask, mqtt_client: MqttClient.Client):
+
+		self.lights = {}
+		for name, lamp_ids in lamp_config.items():
+			self.lights[name] = (CeilingLights(name, lamp_ids, mqtt_client))
+
 		self.setup_routes( app )
+
+	def setup_routes( self, app: Flask ):
+		@app.route( f'/ceiling/<string:name>/brightness/<int:brightness>')
+		def ceiling_lights_brightness( name: str, brightness: int ):
+			self.lights[name].send_to_all_lamps( f'{{ "brightness": "{brightness}" }}' )
+			return "Ok", 200
+
+		@app.route( f'/ceiling/<string:name>/temp/<int:color_temp>')
+		def ceiling_lights_color( name: str, color_temp: int ):
+			self.lights[name].send_to_all_lamps( f'{{ "color_temp": "{color_temp}" }}' )
+			return "Ok", 200
+
+	def produce_main_page_content(self):
+		return "<hr />".join([light.produce_main_page_content() for light in self.lights.values()])
+
+
+class CeilingLights:
+	def __init__(self, name: str, lamp_ids: list[str], mqtt_client: MqttClient.Client):
 		self.mqtt_client = mqtt_client
+		self.name = name
+		self.lamp_ids = lamp_ids
 
-		self.lamp_ids = [
-			'0x680ae2fffe6a2ac5',
-			'0xbc33acfffe59a606',
-		]
-		self.woodlamp = woodlamp
-		self.light_mode_to_restore = None
-
-	@staticmethod
-	def produce_main_page_content():
-		return """
+	def produce_main_page_content(self):
+		return f"""
 			<details>
 				<summary>
-					<button onclick="fetch('ceiling/brightness/254')">On</button>
-					<button onclick="fetch('ceiling/brightness/127')">Dim</button>
-					<button onclick="fetch('ceiling/brightness/0')">Off</button>
+					<button onclick="fetch('ceiling/{self.name}/brightness/254')">On</button>
+					<button onclick="fetch('ceiling/{self.name}/brightness/127')">Dim</button>
+					<button onclick="fetch('ceiling/{self.name}/brightness/0')">Off</button>
 				</summary> 
-				<br /><input type=range min=250 max=454 onchange="fetch(`ceiling/temp/${this.value}`)" />
-				<br /><input type=range min=0 max=254 onchange="fetch(`ceiling/brightness/${this.value}`)" />
+				<br /><input type=range min=250 max=454 onchange="fetch(`ceiling/{self.name}/temp/${{this.value}}`)" />
+				<br /><input type=range min=0 max=254 onchange="fetch(`ceiling/{self.name}/brightness/${{this.value}}`)" />
 			</details>
 		"""
 
@@ -36,14 +53,3 @@ class CeilingLights:
 				topic=f"zigbee2mqtt/{lamp_id}/set",
 				payload=payload
 			)
-
-	def setup_routes( self, app: Flask ):
-		@app.route( '/ceiling/brightness/<int:brightness>' )
-		def ceiling_lights_brightness( brightness: int ):
-			self.send_to_all_lamps( f'{{ "brightness": "{brightness}" }}' )
-			return "Ok", 200
-
-		@app.route( '/ceiling/temp/<int:color_temp>' )
-		def ceiling_lights_color( color_temp: int ):
-			self.send_to_all_lamps( f'{{ "color_temp": "{color_temp}" }}' )
-			return "Ok", 200
--- Server.py
+++ Server.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 import MqttClient
 from Alarm import Alarm
-from CeilingLights import CeilingLights
+from CeilingLights import CeilingLightsCollection
 from Scheduler import make_scheduler
 from Woodlamp import Woodlamp
 
@@ -13,7 +13,33 @@ mqtt_client: MqttClient.Client = MqttClient.get_client()
 
 woodlamp = Woodlamp( app, '192.168.178.26' )
 alarm = Alarm( app, woodlamp )
-ceiling = CeilingLights( app, mqtt_client, woodlamp )
+
+ceiling = CeilingLightsCollection(
+	{
+		"living room": [
+			"0x2c1165fffe9552aa",
+			"0x2c1165fffe97a7ca",
+			"0x2c1165fffe8a6188",
+			"0x50325ffffeaef44e",
+			"0x2c1165fffe8f6498",
+			"0x2c1165fffe954329"
+		],
+		"hall": [
+			"0x680ae2fffe6a2ac5",
+			"0xbc33acfffe59a606"
+		],
+		"bedroom": [
+			"0x2c1165fffe875fb7",
+			"0x50325ffffed2342b",
+			"0x50325ffffebf9de2",
+			"0x2c1165fffe2cb9bb",
+			"0x50325ffffebc506f",
+			"0x2c1165fffe8fa481"
+		]
+	},
+	app,
+	mqtt_client
+)
 
 controllers = [ woodlamp, alarm, ceiling ]
 
