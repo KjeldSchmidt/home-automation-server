a70cb1f976bf8b53c9e979498938157b616216d4
Kjeld Schmidt <contact@kjeld-schmidt.com>
2022-05-27 05:52:04 +0200

    Now taking device config from dedicated file

---
+++ Devices.py
@@ -0,0 +1,21 @@
+class Devices:
+    remotes = (["0x5c0272fffeb364e8"],)
+    ceiling_lamps = {
+        "living room": [
+            "0x2c1165fffe9552aa",
+            "0x2c1165fffe97a7ca",
+            "0x2c1165fffe8a6188",
+            "0x50325ffffeaef44e",
+            "0x2c1165fffe8f6498",
+            "0x2c1165fffe954329",
+        ],
+        "hall": ["0x680ae2fffe6a2ac5", "0xbc33acfffe59a606"],
+        "bedroom": [
+            "0x2c1165fffe875fb7",
+            "0x50325ffffed2342b",
+            "0x50325ffffebf9de2",
+            "0x2c1165fffe2cb9bb",
+            "0x50325ffffebc506f",
+            "0x2c1165fffe8fa481",
+        ],
+    }
--- Server.py
+++ Server.py
@@ -5,49 +5,27 @@ from Alarm import Alarm
 from CeilingLights import CeilingLightsCollection
 from Scheduler import make_scheduler
 from Woodlamp import Woodlamp
+from Devices import Devices
 
-app = Flask( __name__ )
-make_scheduler( app )
+app = Flask(__name__)
+make_scheduler(app)
 
 mqtt_client: MqttClient.Client = MqttClient.get_client()
 
-woodlamp = Woodlamp( app, '192.168.178.26' )
-alarm = Alarm( app, woodlamp )
+woodlamp = Woodlamp(app, "192.168.178.26")
+alarm = Alarm(app, woodlamp)
 
-ceiling = CeilingLightsCollection(
-	{
-		"living room": [
-			"0x2c1165fffe9552aa",
-			"0x2c1165fffe97a7ca",
-			"0x2c1165fffe8a6188",
-			"0x50325ffffeaef44e",
-			"0x2c1165fffe8f6498",
-			"0x2c1165fffe954329"
-		],
-		"hall": [
-			"0x680ae2fffe6a2ac5",
-			"0xbc33acfffe59a606"
-		],
-		"bedroom": [
-			"0x2c1165fffe875fb7",
-			"0x50325ffffed2342b",
-			"0x50325ffffebf9de2",
-			"0x2c1165fffe2cb9bb",
-			"0x50325ffffebc506f",
-			"0x2c1165fffe8fa481"
-		]
-	},
-	app,
-	mqtt_client
-)
+ceiling = CeilingLightsCollection(Devices.ceiling_lamps, app, mqtt_client)
 
-controllers = [ woodlamp, alarm, ceiling ]
+controllers = [woodlamp, alarm, ceiling]
 
 
-@app.route( '/' )
+@app.route("/")
 def main_page():
-	return '<hr />'.join( [ controller.produce_main_page_content() for controller in controllers ] )
+    return "<hr />".join(
+        [controller.produce_main_page_content() for controller in controllers]
+    )
 
 
-if __name__ == '__main__':
-	app.run( host='0.0.0.0' )
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
