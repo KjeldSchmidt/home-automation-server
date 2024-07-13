e9f87d8e58a2344ff137dcdb3e4449f5e9cfb180
Kjeld Schmidt <kjeld.schmidt@thoughtworks.com>
2024-07-13 11:53:02 +0200

    use lamp names consistent with Zigbee friendly group names

--- Devices.py
+++ Devices.py
@@ -1,7 +1,7 @@
 class Devices:
     remotes = (["0x5c0272fffeb364e8"],)
     ceiling_lamps = {
-        "living room": [
+        "ceiling-livingroom": [
             "0x2c1165fffe9552aa",
             "0x2c1165fffe97a7ca",
             "0x2c1165fffe8a6188",
@@ -9,8 +9,8 @@ class Devices:
             "0x2c1165fffe8f6498",
             "0x2c1165fffe954329",
         ],
-        "hall": ["0x680ae2fffe6a2ac5", "0xbc33acfffe59a606"],
-        "bedroom": [
+        "ceiling-hallway": ["0x680ae2fffe6a2ac5", "0xbc33acfffe59a606"],
+        "ceiling-bedroom": [
             "0x2c1165fffe875fb7",
             "0x50325ffffed2342b",
             "0x50325ffffebf9de2",
