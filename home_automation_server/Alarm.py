f989fda619e11ee6554ccd40d67d821d8079e3b6
Kjeld Schmidt <contact@kjeld-schmidt.com>
2025-02-27 12:53:26 +0100

    extract template for alarm instead of generating html inline

--- home_automation_server/Alarm.py
+++ home_automation_server/Alarm.py
@@ -1,11 +1,12 @@
 from uuid import uuid1
 
 from apscheduler.job import Job
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 from flask_apscheduler import APScheduler
 
 import Scheduler
 from Device.EspNeopixelLight import EspNeopixelLight
+from GuiElement import GuiElement
 from TimeFunctions import get_next_valid_time, local_time_today
 
 
@@ -16,21 +17,8 @@ class Alarm(GuiElement):
         self.setup_routes(app)
 
     def get_frontend_html(self):
-        alarms = [job for job in self.scheduler.get_jobs() if job.id.startswith("alarm::")]
-        alarm_elements = [self.make_alarm_component(alarm) for alarm in alarms]
-        set_alarms = "<br />".join(alarm_elements)
-        return f"""
-            <form class="alarm-form" method="post" action="/alarm">
-                <input type="time" name="time" />
-                <input type="submit" value="Set Alarm" />
-                <br />
-                {set_alarms}
-            </form>
-        """
-
-    @staticmethod
-    def make_alarm_component(job: Job):
-        return f"""{local_time_today( job.next_run_time )} <a href="/alarm/{job.id}/delete"> X </a>"""
+        active_alarms = [job for job in self.scheduler.get_jobs() if job.id.startswith("alarm::")]
+        return render_template("alarm.html", active_alarms=active_alarms, local_time_today=local_time_today)
 
     def wake_up(self):
         self.esp_neopixel_light.set_mode("WakeUp")
