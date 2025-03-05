350207c50bdce08584368db840d7499c4c96e726
Kjeld Schmidt <contact@kjeld-schmidt.com>
2025-03-05 11:12:05 +0100

    Move user config to new dedicated UserConfig module

--- home_automation_server/env-example.py
+++ home_automation_server/env-example.py
@@ -4,4 +4,4 @@ SPOTIFY_APP_ID = "PLACEHOLDER_VALUE"
 SPOTIFY_APP_SECRET = "PLACEHOLDER_VALUE"
 SPOTIFY_REFRESH_TOKEN = "PLACEHOLDER_VALUE"
 
-USER_CONFIG_MODULE = "DeviceGroup.Name"  # replace "Name" with Jan/Kjeld
+USER_CONFIG_MODULE = "UserConfig.Name"  # replace "Name" with Jan/Kjeld
